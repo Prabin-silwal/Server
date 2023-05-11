45bd57f36989af04984d0c6baf488da4a669e340
Prabin Silwal <psilwal50@gmail.com>
2023-05-11 13:29:04 +1000

    upload to cloud and display from cloud are done

--- server.js
+++ server.js
@@ -1,67 +1,89 @@
 const express = require('express');
+const cors = require('cors');
+const { default: mongoose } = require('mongoose');
+const bodyParser = require('body-parser');
 const app = express();
-const port = 8000;
+const port = process.env || 8000;
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
-const uri = "mongodb+srv://psilwal50:y4AXI1Lbh5Gu6LT8@cluster0.po3udqy.mongodb.net/?retryWrites=true&w=majority";
+app.use(cors());
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+// url for the database
+const uri = "mongodb+srv://psilwal50:S2EhyyJcIb88hEW6@assignment.lhchzfr.mongodb.net/Assignment2";
 
-// Create a MongoClient with a MongoClientOptions object to set the Stable API version
-const client = new MongoClient(uri, {
-  serverApi: {
-    version: ServerApiVersion.v1,
-    strict: true,
-    deprecationErrors: true,
-  }
-});
+// mongodb connection
+const MongoClient = require('mongodb').MongoClient;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true  });
+// body-parser
+client.connect()
+  .then(() => {
+    console.log('Connected to MongoDB');
+    const server = app.listen(port, () => {
+  
+      console.log(`Server running at http://localhost:${port}`);
+    });
+    
+  })
+  .catch((err) => {
+    console.error(err);
+  });
 
-var database;
-
-async function run() {
-  try {
-    // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
-    // Get a reference to the database
-    database = client.db('assignment');
-    console.log("Connected to the database");
-  } catch (e) {
-    console.error("Failed to connect to the database:", e);
-  }
-}
-
-run();
-
-app.get('/', (req, res) => {
+ // routes
+app.get('/api', (req, res) => {
   res.send('Hello World!');
 });
 
-app.get('/scan', async(req, res) => {
-await  database.collection('scannedData').find().toArray((err, result) => {
-    if (err) {
-      console.log('result');
-      console.error("Failed to fetch data from the database:", err);
-      return res.status(500).send("Failed to fetch data from the database");
-    }
+// get all albums
+app.get('/api/scan', async(req, res) => {
+  try {
+    const collection = client.db('Assignment2').collection('scanned_data');
+    const result = await collection.find().toArray();
     console.log(result);
     res.json(result);
-  });
-  // const hero = await database.collection('scan_data').find().toArray()((err, result) => { 
-  //   console.log(result);
-  // });
-  // res.send(hero);
+  } catch (err) {
+    console.error("Failed to fetch data from the database:", err);
+    res.status(500).send("Failed to fetch data from the database");
+  }
 });
 
+//add the scanned data to the database
+app.post('/api/uploadToCloud', async (req, res) => { 
+  const data = req.body;
 
-
-app.post('/insertscandata', (req, res) => {
-  const { field1, field2, field3, field4, field5 } = req.body;
-  
-  database.collection('scannedData').insertOne({ field1, field2, field3, field4, field5 }, (err, result) => {
-    if (err) throw err;
-    console.log("Data inserted successfully");
-    res.send("Data inserted successfully");
+  client.db('Assignment2').collection('albums').insertMany(data).then(result => {
+    
+    console.log(`${result.insertedCount} document inserted`);
+    res.status(200).json(result);
+  }).catch(err => { 
+    res.status(500).send("Failed to insert data into the database");
   });
 });
 
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
+//fetch the data from the database
+app.get('/api/displayCloudData', async(req, res) => {
+  try {
+    const collection = client.db('Assignment2').collection('albums');
+    const result = await collection.find().toArray();
+    console.log(result);
+    res.json(result); 
+  } catch (err) {
+    console.error("Failed to fetch data from the database:", err);
+    res.status(500).send("Failed to fetch data from the database");
+  }
+});
+
+// delete data from cloud
+
+app.delete('/api/delete', async (req, res) => {
+  try {
+    const collection = client.db('Assignment2').collection('albums'); 
+    const result = await db.collection('albums').deleteMany({});
+    res.status(200).json({ message: `${result.deletedCount} documents deleted from ${collection} collection` });
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ message: 'An error occurred while deleting documents from the collection' });
+  } finally {
+    await client.close();
+  }
 });
